7f8f29266004ad3224fbe4c0fecfcc92d410611b
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-07-02 10:52:14 +0530

    Connecting jokes database

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,4 +1,8 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import {json} from "@remix-run/node";
+
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
 
 import { Outlet, Link } from "@remix-run/react";
 import stylesUrl from "~/styles/jokes.css";
@@ -7,7 +11,25 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export default function jokes() {
+type LoaderData = {jokelistItem:Array<{id:String,name:String}>};
+
+export const loader:LoaderFunction =async () => {
+  const data :LoaderData = {
+    jokelistItem:await db.joke.findMany(
+      {
+        take:5,
+        select:{id:true,name:true},
+        orderBy:{createdAt:"desc"}
+
+      }),
+  };
+  return json(data);
+};
+
+export default function Jokes() {
+  console.log("some thins");
+  const data = useLoaderData<LoaderData>();
+  console.log(data);
   return (
     <div className="jokes-layout">
   <header className="jokes-header">
@@ -26,12 +48,23 @@ export default function jokes() {
       </header>  <main className="jokes-main">
         <div className="container">
           <div className="jokes-list">
+            
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
+
+            {data.jokelistItem.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+            ))}
+                
               <li>
                 <Link to="some-joke-id">Hippo</Link>
               </li>
+          
+             
+             
             </ul>
             <Link to="create" className="button">
               Add your own
