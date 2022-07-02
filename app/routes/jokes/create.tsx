7c8adee75f98947cbbe90ebc7cd07a8bdf25a27f
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-07-02 11:36:33 +0530

    Functionality to add new joke

--- app/routes/jokes/create.tsx
+++ app/routes/jokes/create.tsx
@@ -1,4 +1,25 @@
-import React from 'react'
+import type {ActionFunction} from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+
+
+export const action:ActionFunction =async ({request,}) => {
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+
+  if(typeof name!=="string"|| typeof content!=="string"){
+    throw new Error("Form not sumitted correctly.");
+  }
+
+  const fields = {name,content};
+
+  const joke = await db.joke.create({data:fields});
+  return redirect(`/jokes/${joke.id}`);
+
+} 
+
+
 
 export default function create() {
   return (
