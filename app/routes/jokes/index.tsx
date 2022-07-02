2324988d2b5056baa970e9168f5c7c7d8219168c
Pranav Choudhary <2018184@iiitdmj.ac.in>
2022-07-02 11:28:09 +0530

    Show Random Joke

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,11 +1,34 @@
-export default function index() {
+import { db } from "~/utils/db.server";
+import { Joke } from "@prisma/client";
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData,Link } from "@remix-run/react"
+
+type LoaderData = {randomJoke:Joke};
+
+export const loader:LoaderFunction =async () => {
+  const count = await db.joke.count();
+  const randomNumber = Math.floor(Math.random()*count);
+
+  const [randomJoke] = await db.joke.findMany({
+    take:1,
+    skip: randomNumber,
+  });
+  const data: LoaderData = {randomJoke};
+  return json(data);
+}
+
+
+export default function Index() {
+  const data = useLoaderData<LoaderData>();
+
+
   return (
     <div>
     <p>Here's a random joke:</p>
-    <p>
-      I was wondering why the frisbee was getting bigger,
-      then it hit me.
-    </p>
+    <p>{data.randomJoke.content}</p>
+    <Link to={data.randomJoke.id}>
+      "{data.randomJoke.name}" Permalink
+    </Link>
   </div>
   )
 }
